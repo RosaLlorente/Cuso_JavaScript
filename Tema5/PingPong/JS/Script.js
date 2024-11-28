9580ab73e58500b6f54512156d33261f44595712
Rosa Llorente <rosamariallorentemojica.com>
2024-11-29 00:21:16 +0100

    Actualización de ejercicios,tema 5,se ha añadido la relación cinco y traducido el pingpong

--- Tema5/PingPong/JS/Script.js
+++ Tema5/PingPong/JS/Script.js
@@ -1,182 +1,194 @@
 window.onload = () => {
-    const svgPadre = document.querySelector("svg");
-    const limiteX = 1000;
-    const limiteY = 500;
+    const svgFather = document.querySelector("svg");
+    const limitX = 1000;
+    const limitY = 500;
 
-    // Variables de puntuación y elementos HTML
-    let puntuacionJugador1 = 0;
-    let puntuacionJugador2 = 0;
-    const spanJugador1 = document.querySelectorAll("p span")[0];
-    const spanJugador2 = document.querySelectorAll("p span")[1];
+    //Score variables and HTML elements
+    let player1score = 0;
+    let player2score = 0;
+    const spanPlayer1 = document.querySelectorAll("p span")[0];
+    const spanPlayer2 = document.querySelectorAll("p span")[1];
 
-    spanJugador1.textContent = puntuacionJugador1;
-    spanJugador2.textContent = puntuacionJugador2;
+    spanPlayer1.textContent = player1score;
+    spanPlayer2.textContent = player2score;
 
-    var Pelota = new pelota(50, 50, 3, 3, limiteX, limiteY, 10, svgPadre, actualizarPuntuacion);
-    var rectangulo1 = new Rectangulo(0, 200, 30, 100, svgPadre);
-    var rectangulo2 = new Rectangulo(970, 200, 30, 100, svgPadre);
+    var Ball = new ball(50, 50, 3, 3, limitX, limitY, 10, svgFather, updateScore);
+    var rectangle1 = new Rectangle(0, 200, 30, 100, svgFather);
+    var rectangle2 = new Rectangle(970, 200, 30, 100, svgFather);
 
-    Pelota.mover([rectangulo1, rectangulo2]);
+    Ball.move([rectangle1, rectangle2]);
 
-    MusicaFondo();
+    //PROGRAM
+    MusicBackground();
 
+    //Eventos para asignar teclas de juego
+    //Jugador 1
     document.addEventListener("keydown", (event) => 
     {
         if (event.key === "w") 
         {
-            if (rectangulo1.posicionY > 0) 
+            if (rectangle1.positionY > 0) 
             {
-                rectangulo1.posicionY -= 10;
-                rectangulo1.actualizarPosicion();
+                rectangle1.positionY -= 10;
+                rectangle1.updatePosition();
             }
         } 
         else if (event.key === "s") 
         {
-            if (rectangulo1.posicionY + rectangulo1.alto < limiteY) 
+            if (rectangle1.positionY + rectangle1.height < limitY) 
             {
-                rectangulo1.posicionY += 10;
-                rectangulo1.actualizarPosicion();
+                rectangle1.positionY += 10;
+                rectangle1.updatePosition();
             }
         } 
+
+        //Jugador 2
         else if (event.key === "ArrowUp") 
         {
-            if (rectangulo2.posicionY > 0) 
+            if (rectangle2.positionY > 0) 
             {
-                rectangulo2.posicionY -= 10;
-                rectangulo2.actualizarPosicion();
+                rectangle2.positionY -= 10;
+                rectangle2.updatePosition();
             }
         } 
         else if (event.key === "ArrowDown")
             {
-            if (rectangulo2.posicionY + rectangulo2.alto < limiteY) 
+            if (rectangle2.positionY + rectangle2.height < limitY) 
             {
-                rectangulo2.posicionY += 10;
-                rectangulo2.actualizarPosicion();
+                rectangle2.positionY += 10;
+                rectangle2.updatePosition();
             }
         }
     });
 
-    function actualizarPuntuacion(jugador)
+    //Función para actualizar la puntuación
+    function updateScore(player)
     {
-        if (jugador === 1) 
+        if (player === 1) 
         {
-            puntuacionJugador1++;
-            spanJugador1.textContent = puntuacionJugador1;
+            player1score++;
+            spanPlayer1.textContent = player1score;
         } 
-        else if (jugador === 2) 
+        else if (player === 2) 
         {
-            puntuacionJugador2++;
-            spanJugador2.textContent = puntuacionJugador2;
+            player2score++;
+            spanPlayer2.textContent = player2score;
         }
-        SonidoPunto();
+        SoundPoint();
     }
 };
-class pelota 
+
+//CLASES
+//Class that creates a ball
+class ball 
 {
-    constructor(posicionX, posicionY, velocidadx, velocidady, limiteX, limiteY, radio, svgPadre, actualizarPuntuacionCallback) 
+    constructor(positionX, positionY, speedx, speedy, limitX, limitY, radio, svgFather, updateScoreCallback) 
     {
-        this.posicionX = posicionX;
-        this.posicionY = posicionY;
-        this.velocidadx = velocidadx;
-        this.velocidady = velocidady;
-        this.limiteX = limiteX;
-        this.limiteY = limiteY;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.speedx = speedx;
+        this.speedy = speedy;
+        this.limitX = limitX;
+        this.limitY = limitY;
         this.radio = radio;
-        this.actualizarPuntuacion = actualizarPuntuacionCallback;
-        this.crearTag(svgPadre);
+        this.updateScore = updateScoreCallback;
+        this.crearTag(svgFather);
     }
 
-    crearTag(svgPadre) 
+    crearTag(svgFather) 
     {
-        this.pelota = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        this.pelota.setAttribute("cx", this.posicionX);
-        this.pelota.setAttribute("cy", this.posicionY);
-        this.pelota.setAttribute("r", this.radio);
-        this.pelota.setAttribute("stroke", "black");
-        this.pelota.setAttribute("stroke-width", 1);
-        this.pelota.setAttribute("fill", color());
-        svgPadre.appendChild(this.pelota);
+        this.ball = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        this.ball.setAttribute("cx", this.positionX);
+        this.ball.setAttribute("cy", this.positionY);
+        this.ball.setAttribute("r", this.radio);
+        this.ball.setAttribute("stroke", "black");
+        this.ball.setAttribute("stroke-width", 1);
+        this.ball.setAttribute("fill", color());
+        svgFather.appendChild(this.ball);
     }
 
-    mover(rectangulos) 
+    move(Rectangles)  //Method for interaction with the x axes and rectangles
     {
         setInterval(() => 
         {
-            this.posicionX += this.velocidadx;
-            this.pelota.setAttribute("cx", this.posicionX);
+            this.positionX += this.speedx;
+            this.ball.setAttribute("cx", this.positionX);
 
-            this.posicionY += this.velocidady;
-            this.pelota.setAttribute("cy", this.posicionY);
+            this.positionY += this.speedy;
+            this.ball.setAttribute("cy", this.positionY);
 
-            if ((this.posicionY + this.radio) > this.limiteY || (this.posicionY - this.radio) < 0) 
+            if ((this.positionY + this.radio) > this.limitY || (this.positionY - this.radio) < 0) 
             {
-                this.velocidady *= -1;
-                SonidoPelota();
+                this.speedy *= -1;
+                SoundBall();
             }
 
-            if (this.posicionX - this.radio < 0) 
+            if (this.positionX - this.radio < 0) 
             {
-                // Punto para jugador 2
-                this.actualizarPuntuacion(2);
-                this.reiniciar();
+                // Point for player 2
+                this.updateScore(2);
+                this.reset();
             } 
-            else if (this.posicionX + this.radio > this.limiteX) 
+            else if (this.positionX + this.radio > this.limitX) 
             {
-                // Punto para jugador 1
-                this.actualizarPuntuacion(1);
-                this.reiniciar();
+                // Point for player 1
+                this.updateScore(1);
+                this.reset();
             }
 
-            rectangulos.forEach((rect) => 
+            Rectangles.forEach((rect) => 
             {
-                if (this.posicionX - this.radio < rect.posicionX + rect.ancho && this.posicionX + this.radio > rect.posicionX && this.posicionY + this.radio > rect.posicionY && this.posicionY - this.radio < rect.posicionY + rect.alto) 
+                if (this.positionX - this.radio < rect.positionX + rect.width && this.positionX + this.radio > rect.positionX && this.positionY + this.radio > rect.positionY && this.positionY - this.radio < rect.positionY + rect.height) 
                 {
-                    this.velocidadx *= -1;
-                    SonidoPelota();
+                    this.speedx *= -1;
+                    SoundBall();
                 }
             });
         }, 20);
     }
 
-    reiniciar() 
+    reset() 
     {
-        this.posicionX = this.limiteX / 2;
-        this.posicionY = this.limiteY / 2;
-        this.velocidadx *= -1; // Cambia dirección de la pelota
-        this.pelota.setAttribute("cx", this.posicionX);
-        this.pelota.setAttribute("cy", this.posicionY);
+        this.positionX = this.limitX / 2;
+        this.positionY = this.limitY / 2;
+        this.speedx *= -1; // Cambia dirección de la ball
+        this.ball.setAttribute("cx", this.positionX);
+        this.ball.setAttribute("cy", this.positionY);
     }
 }
 
-class Rectangulo 
+//Class that creates a rectangle
+class Rectangle 
 {
-    constructor(posicionX, posicionY, ancho, alto, svgPadre) 
+    constructor(positionX, positionY, width, height, svgFather) 
     {
-        this.posicionX = posicionX;
-        this.posicionY = posicionY;
-        this.ancho = ancho;
-        this.alto = alto;
-        this.crearTag(svgPadre);
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+        this.crearTag(svgFather);
     }
 
-    crearTag(svgPadre) 
+    crearTag(svgFather) 
     {
-        this.rectangulo = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        this.rectangulo.setAttribute("x", this.posicionX);
-        this.rectangulo.setAttribute("y", this.posicionY);
-        this.rectangulo.setAttribute("width", this.ancho);
-        this.rectangulo.setAttribute("height", this.alto);
-        this.rectangulo.setAttribute("stroke", "black");
-        this.rectangulo.setAttribute("stroke-width", 1);
-        this.rectangulo.setAttribute("fill", color());
-        svgPadre.appendChild(this.rectangulo);
+        this.Rectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        this.Rectangle.setAttribute("x", this.positionX);
+        this.Rectangle.setAttribute("y", this.positionY);
+        this.Rectangle.setAttribute("width", this.width);
+        this.Rectangle.setAttribute("height", this.height);
+        this.Rectangle.setAttribute("stroke", "black");
+        this.Rectangle.setAttribute("stroke-width", 1);
+        this.Rectangle.setAttribute("fill", color());
+        svgFather.appendChild(this.Rectangle);
     }
-    actualizarPosicion() 
+    updatePosition()
     {
-        this.rectangulo.setAttribute("y", this.posicionY);
+        this.Rectangle.setAttribute("y", this.positionY);
     }
 }
 
+//FUNCTIONS
+//Function to assign a random color
 function color() 
 {
     const r = Math.floor(Math.random() * 256);
@@ -185,26 +197,32 @@ function color()
 
     return "rgb(" + r + "," + g + "," + b + ")";
 }
-function MusicaFondo() 
+
+//Function to insert background music
+function MusicBackground() 
 {
-    var KeySound = new Audio('Sonido/Patty no Theme.mp3');
+    var KeySound = new Audio('Sound/Patty no Theme.mp3');
     KeySound.play();
     KeySound.loop = true;
     KeySound.volume = 0.08;
-    document.body.addEventListener('keydown', () => 
+    document.body.addEventListener('keydown', () =>
     {
-        KeySound.play().catch((error) => console.error("Error reproduciendo el sonido:", error));
+        KeySound.play().catch((error) => console.error("Error playing sound:", error));
     });
 }
-function SonidoPelota() 
+
+//Function to apply sound effects to the ball
+function SoundBall() 
 {
-    var KeySound = new Audio('Sonido/ping-pong-ball-100074 (mp3cut.net).mp3');
+    var KeySound = new Audio('Sound/ping-pong-ball-100074 (mp3cut.net).mp3');
     KeySound.play();
     KeySound.volume = 2.0;
 }
-function SonidoPunto() 
+
+//Function to apply sound effects to points
+function SoundPoint() 
 {
-    var KeySound = new Audio('Sonido/retro-blip-236676.mp3');
+    var KeySound = new Audio('Sound/retro-blip-236676.mp3');
     KeySound.play();
     KeySound.volume = 1.0;
 }
